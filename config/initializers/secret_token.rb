6e42d5af87783731795ddc472c3bcf2a69f4a46d
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-11-17 02:52:06 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jcsautosales::Application.config.secret_key_base = '825927e2bade2d767451f08c188694615607f961e9f5f375614850954f3eb0fdf5b05b7efc52d1895ef4da3d7119a579fcc57b7cbdc6c0fef01542d8f391bc17'
